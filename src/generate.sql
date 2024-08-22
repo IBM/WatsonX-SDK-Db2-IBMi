8f3326bc0ee3843d89fc15dac8f8c9c8364a096c
worksofliam <mrliamallan@live.co.uk>
2024-08-22 14:07:57 -0400

    Add additional output
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/generate.sql
+++ src/generate.sql
@@ -8,6 +8,9 @@ create or replace function watsonx.generate(
   no external action
   set option usrprf = *user, dynusrprf = *user, commit = *none
 begin
+
+  declare response_header Varchar(10000) CCSID 1208;
+  declare response_message Varchar(10000) CCSID 1208;
   declare watsonx_response Varchar(10000) CCSID 1208;
   declare needsNewToken char(1) default 'Y';
 
@@ -16,26 +19,28 @@ begin
     return '*PLSAUTH';
   end if;
 
-  -- TODO: consider using verbose to we can capture errors
-  -- TODO: store the result into a response variable, then parse after
-
   if parameters is null then
     set parameters = watsonx.parameters(max_new_tokens => 100, time_limit => 1000);
   end if;
-  
-  select ltrim("generated_text") into watsonx_response
-  from json_table(QSYS2.HTTP_POST(
+
+  select RESPONSE_MESSAGE, RESPONSE_HTTP_HEADER
+  into response_message, response_header
+  from table(HTTP_POST_VERBOSE(
     watsonx.geturl('/text/generation'),
     json_object('model_id': model_id, 'input': text, 'parameters': parameters format json, 'space_id': watsonx.spaceid),
-    json_object('headers': json_object('Authorization': 'Bearer ' concat watsonx.JobBearerToken, 'Content-Type': 'application/json', 'Accept': 'application/json')) 
-  ), 'lax $.results[*]'
-  columns(
-    "generated_text" varchar(10000) ccsid 1208
-  ));
-
-  if (watsonx_response is null) then
-    return '*ERROR';
-  end if;
+    json_object('headers': json_object('Authorization': 'Bearer ' concat watsonx.JobBearerToken, 'Content-Type': 'application/json', 'Accept': 'application/json'))
+  )) x;
   
-  return watsonx_response;
+  
+  -- select ltrim("generated_text") into watsonx_response
+  -- from json_table(response_message, 'lax $.results[*]'
+  -- columns(
+  --   "generated_text" varchar(10000) ccsid 1208
+  -- ));
+
+  -- if (watsonx_response is null) then
+  --   return '*ERROR';
+  -- end if;
+  
+  return json_object('response_message': response_message format json, 'response_header': response_header format json);
 end;
